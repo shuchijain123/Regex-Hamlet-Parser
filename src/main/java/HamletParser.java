b8c93f7b514434f479e6440b88f08c67c068bd11
Shuchi Jain <shuchijain@zipcoders-MacBook-Pro.local>
2019-03-18 17:58:12 -0400

    committing parser regx Lab

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,10 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,32 +12,144 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    Pattern pattern1 = null;
+    Matcher matcher1 = null;
+    Matcher matcher2 = null;
+    String line = null;
+    String newline=null;
 
-    public HamletParser(){
+
+
+    Scanner myscanner = null;
+
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
+
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
+
         return hamletData;
     }
+
+    public void setHamletData() {
+
+        this .hamletData=hamletData;
+    }
+
+
+
+    public String ChangeStringInFile(String oldString, String newString) {
+        String myfile = loadFile();
+
+        String patternString = "(?i)"+oldString;
+
+        pattern1 = Pattern.compile(patternString);
+
+        matcher1 = pattern1.matcher(myfile);
+        if (matcher1.find()) {
+
+            newline = matcher1.replaceAll(newString);
+
+
+
+
+        }
+        return newline;
+    }
+
+
+    public Boolean readStringInFile(String myString) {
+
+        Boolean ans = true;
+        String myReadFile="";
+        String patternString = "(?i)"+myString;
+
+        if (myReadFile.contains(myString)) {
+            ans = false;
+        } else
+            ans = true;
+
+        return ans;
+
+    }
+
+    public void writeToFile(String s){
+       try {
+           ClassLoader classLoader = getClass().getClassLoader();
+           File file = new File(classLoader.getResource("hamlet_Output.txt").getFile());
+           FileOutputStream fooStream = new FileOutputStream(file, false); // true to append
+           // false to overwrite.
+           byte[] myBytes = s.getBytes();
+           fooStream.write(myBytes);
+           fooStream.close();
+       }
+       catch (IOException e){
+           e.printStackTrace(System.out);
+       }
+    }
+
+
+
+
+    public void copyFiles(){
+        try {
+            ClassLoader classLoader = getClass().getClassLoader();
+           File file = new File(classLoader.getResource("hamlet_backup.txt").getFile());
+            classLoader = getClass().getClassLoader();
+         File originalfile = new File(classLoader.getResource("hamlet.txt").getFile());
+
+
+
+            FileInputStream inFile = new FileInputStream(file);
+            int fileLength =(int)file.length();
+
+            byte Bytes[]=new byte[fileLength];
+
+            System.out.println("File size is: " + inFile.read(Bytes));
+
+            String file1 = new String(Bytes);
+            System.out.println("File content is:\n" + file1);
+
+            FileOutputStream outFile = new FileOutputStream(originalfile);
+
+            for (int i = 0; i<fileLength; i++)
+            {
+                outFile.write(Bytes[i]);
+            }
+
+            System.out.println("File copied.");
+            //close files
+            inFile.close();
+            outFile.close();
+
+        }
+        catch (IOException e){
+            e.printStackTrace(System.out);
+        }
+    }
+
+
+
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,31 +1,90 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class HamletParserTest {
     private String hamletText;
     private HamletParser hamletParser;
+    String expectedleon;
+
+
 
     @Before
     public void setUp() {
         this.hamletParser = new HamletParser();
         this.hamletText = hamletParser.getHamletData();
+
+
+
     }
+
 
     @Test
     public void testChangeHamletToLeon() {
+
+       String expectedleon = hamletParser.ChangeStringInFile("Hamlet", "Leon");
+       System.out.println(expectedleon);
+
+
+        Assert.assertFalse(expectedleon.contains("Hamlet"));
+
+
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+
+
+        String expected = hamletParser.ChangeStringInFile("Horatio", "Tariq");
+        System.out.println(expected);
+        Assert.assertFalse(expected.contains("Horatio"));
+
     }
 
     @Test
     public void testFindHoratio() {
+
+       String expectedstring= hamletParser.ChangeStringInFile("Horatio", "Tariq");
+        Boolean actual = hamletParser.readStringInFile("Horatio");
+        Boolean expected=true;
+        System.out.println(expectedstring);
+
+        Assert.assertEquals(expected,actual);
+
+
+
     }
 
     @Test
     public void testFindHamlet() {
+
+
+        String expectedstring= hamletParser.ChangeStringInFile("Hamlet", "Leon");
+        Boolean actual = hamletParser.readStringInFile("Hamlet");
+        Boolean expected=true;
+        System.out.println(expectedstring);
+
+        Assert.assertEquals(expected,actual);
+
+    }
+    @Test
+    public void testWriteOutput() {
+
+
+        String leonfile = hamletParser.ChangeStringInFile("Hamlet", "Leon");
+        hamletParser.getHamletData();
+        hamletParser.writeToFile(leonfile);
+        String Tariqfile = hamletParser.ChangeStringInFile("Horatio", "Tariq");
+        hamletParser.writeToFile(Tariqfile);
+        System.out.println(Tariqfile);
+
+
+
+
     }
 }
